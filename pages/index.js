3c25c182acb33eade100e9af04c852cbeac7bcbe
An Tr <andrecee.tnn@gmail.com>
2021-06-20 06:41:48 +0700

    initializing project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Facebook</title>
+      </Head>
+      
+      <main></main>
+    </div>
+  );
+}
